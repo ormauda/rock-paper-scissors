93390d1594e9e36c57d7ff34b510981415163b91
Or Mauda <ormauda@gmail.com>
2020-10-13 19:00:05 +0300

    Fix switch weapon bug

--- Assets/Scripts/Launcher.cs
+++ Assets/Scripts/Launcher.cs
@@ -9,8 +9,10 @@ public class Launcher : MonoBehaviour
     [SerializeField] Projectile[] projectilesPrefabs;
     [SerializeField] float launchSpeed = 10f;
     [SerializeField] float projectileFirePeriod = 1f;
+    [SerializeField] float switchProjectileDelay = 0.2f;
 
     private Coroutine firingCoroutine;
+    private Projectile selectedProjectilePrefab = null;
     private string selectedProjectileButtonName = null;
     private readonly string scissorsButtonName = "Fire1";
     private readonly string paperButtonName = "Fire2";
@@ -44,21 +46,42 @@ public class Launcher : MonoBehaviour
 
     private void Fire()
     {
-        Projectile projectilePrefab = GetProjectileFromInput();
-        //if (projectilePrefab == null)
-        //{
-        //    return;
-        //}
-        if (selectedProjectileButtonName != null && Input.GetButtonUp(selectedProjectileButtonName))
+        var previousProjectilePrefab = selectedProjectilePrefab;
+
+        SetProjectileFromInput();
+
+        if (HasSwitchedProjectile(previousProjectilePrefab) || HasStoppedFiring(previousProjectilePrefab))
         {
+            Debug.Log("stopping coroutine");
             StopCoroutine(firingCoroutine);
-            selectedProjectileButtonName = null;
-        }
-        else if (projectilePrefab != null)
-        {
-            firingCoroutine = StartCoroutine(FireContinuously(projectilePrefab));
         }
 
+        if (HasSwitchedProjectile(previousProjectilePrefab) || HasStartedFiring(previousProjectilePrefab))
+        {
+            Debug.Log("starting coroutine");
+            firingCoroutine = StartCoroutine(FireContinuously(selectedProjectilePrefab));
+        }
+    }
+
+    private bool HasStoppedFiring(Projectile previousProjectile)
+    {
+        if (selectedProjectilePrefab == null && previousProjectile != null)
+            Debug.Log("stopped firing");
+        return selectedProjectilePrefab == null && previousProjectile != null;
+    }
+
+    private bool HasSwitchedProjectile(Projectile previousProjectile)
+    {
+        if (selectedProjectilePrefab != previousProjectile && previousProjectile != null && selectedProjectilePrefab != null)
+            Debug.Log("switching projectile");
+        return selectedProjectilePrefab != previousProjectile && previousProjectile != null && selectedProjectilePrefab != null;
+    }
+
+    private bool HasStartedFiring(Projectile previousProjectile)
+    {
+        if (selectedProjectilePrefab != null && previousProjectile == null)
+            Debug.Log("starting firing");
+        return selectedProjectilePrefab != null && previousProjectile == null;
     }
 
     private IEnumerator FireContinuously(Projectile projectilePrefab)
@@ -72,26 +95,28 @@ public class Launcher : MonoBehaviour
         }
     }
 
-    private Projectile GetProjectileFromInput()
+    private void SetProjectileFromInput()
     {
-        Projectile projectilePrefab = null;
         if (Input.GetButtonDown(scissorsButtonName))
         {
-            projectilePrefab = projectilesPrefabs[0];
+            selectedProjectilePrefab = projectilesPrefabs[0];
             selectedProjectileButtonName = scissorsButtonName;
         }
         else if (Input.GetButtonDown(paperButtonName))
         {
-            projectilePrefab = projectilesPrefabs[1];
+            selectedProjectilePrefab = projectilesPrefabs[1];
             selectedProjectileButtonName = paperButtonName;
         }
         else if (Input.GetButtonDown(rockButtonName))
         {
-            projectilePrefab = projectilesPrefabs[2];
+            selectedProjectilePrefab = projectilesPrefabs[2];
             selectedProjectileButtonName = rockButtonName;
         }
-
-        return projectilePrefab;
+        else if (selectedProjectileButtonName != null && Input.GetButtonUp(selectedProjectileButtonName))
+        {
+            selectedProjectilePrefab = null;
+            selectedProjectileButtonName = null;
+        }
     }
 
     /*
