534f1403854e743dd51ae69e4e359c99e9fc614f
Or Mauda <ormauda@gmail.com>
2020-10-27 21:57:23 +0200

    Add stopwatch

---
+++ Assets/Scripts/Clock.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using TMPro;
+using UnityEngine;
+
+public class Clock : MonoBehaviour
+{
+    float timeSinceStart;
+
+    // Cached references
+    TextMeshProUGUI clockText;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        clockText = GetComponent<TextMeshProUGUI>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        timeSinceStart += Time.deltaTime;
+        clockText.text = timeSinceStart.ToString("F2");
+    }
+
+    string PadWithLeadingZero(int number)
+    {
+        return number.ToString().PadLeft(2, '0');
+    }
+}
