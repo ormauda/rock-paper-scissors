3f10a27308168d1765b42cf6049216fea790754f
Or Mauda <ormauda@gmail.com>
2020-12-13 18:16:25 +0200

    Set correct score and life on restart

--- Assets/Scripts/GameSession.cs
+++ Assets/Scripts/GameSession.cs
@@ -2,6 +2,7 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class GameSession : MonoBehaviour
 {
@@ -9,13 +10,18 @@ public class GameSession : MonoBehaviour
     [SerializeField] int score = 0;
     [SerializeField] int life = 1;
 
+
     // Cached references
     ScreenLoader screenLoader;
+
+    private void Awake()
+    {
+        SetUpSingletone();
+    }
 
     // Start is called before the first frame update
     void Start()
     {
-        SetUpSingletone();
         screenLoader = FindObjectOfType<ScreenLoader>();
     }
 
@@ -46,6 +52,12 @@ public class GameSession : MonoBehaviour
     internal object GetLife()
     {
         return life;
+    }
+
+    public void RestartGame()
+    {
+        SceneManager.LoadScene(0);
+        Destroy(gameObject);
     }
 
     private void SetUpSingletone()
---
+++ Assets/Scripts/Lose Screen/Restart.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Restart : MonoBehaviour
+{
+    // Cached references
+    GameSession gameSession;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        gameSession = FindObjectOfType<GameSession>();
+    }
+
+    public void RestartGame()
+    {
+        gameSession.RestartGame();
+    }
+}
