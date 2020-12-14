1534c8564f961b8c88e787dd909edaffa615e1d9
Or Mauda <ormauda@gmail.com>
2020-12-14 14:19:45 +0200

    Add music player

---
+++ Assets/MusicPlayer.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MusicPlayer : MonoBehaviour
+{
+    private void Awake()
+    {
+        SetUpSingleton();
+    }
+
+    // Start is called before the first frame update
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+
+    private void SetUpSingleton()
+    {
+        int musicPlayersCount = FindObjectsOfType<MusicPlayer>().Length;
+        if (musicPlayersCount > 1)
+        {
+            Destroy(gameObject);
+        }
+        else
+        {
+            DontDestroyOnLoad(gameObject);
+        }
+    }
+}
