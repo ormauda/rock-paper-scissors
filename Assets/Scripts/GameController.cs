a5e3b7db22f56494d81a9af1673a53a80f635716
Or Mauda <ormauda@gmail.com>
2020-12-15 17:37:45 +0200

    Add death sound

--- Assets/Scripts/GameController.cs
+++ Assets/Scripts/GameController.cs
@@ -5,22 +5,26 @@ using UnityEngine.SceneManagement;
 
 public class GameController : MonoBehaviour
 {
+    [SerializeField] float secondsToWaitBeforeLost = 1f;
     [SerializeField] GameObject lostLabel;
 
     // Cached references
     ScreenLoader screenLoader;
 
+    //Cached components
+    AudioSource audioSource;
+
     // Start is called before the first frame update
     void Start()
     {
         screenLoader = FindObjectOfType<ScreenLoader>();
+        audioSource = GetComponent<AudioSource>();
         lostLabel.SetActive(false);
     }
 
     public void Lose()
     {
-        lostLabel.SetActive(true);
-        Time.timeScale = 0;
+        StartCoroutine(PlayLoseSound());
     }
 
     public void RestartGame()
@@ -28,5 +32,13 @@ public class GameController : MonoBehaviour
         lostLabel.SetActive(false);
         Time.timeScale = 1;
         screenLoader.RestartGame();
+    }
+
+    private IEnumerator PlayLoseSound()
+    {
+        Time.timeScale = 0;
+        audioSource.Play();
+        yield return new WaitForSecondsRealtime(secondsToWaitBeforeLost);
+        lostLabel.SetActive(true);
     }
 }
